d24d0dadafe66b3a8ae19cd7993b95dd9c1c2e9a
shsaish006 <shsaish006@gmail.com>
2026-05-14 01:06:59 +0530

    Fix residual SAS token reference in storage outputs

--- modules/storage/outputs.tf
+++ modules/storage/outputs.tf
@@ -1,4 +1,11 @@
-output "sas_token" {
-  value     = data.azurerm_storage_account_blob_container_sas.sas.sas
-  sensitive = true
+output "sa1_id" {
+  value = azurerm_storage_account.sa1.id
+}
+
+output "sa2_id" {
+  value = azurerm_storage_account.sa2.id
+}
+
+output "sa3_id" {
+  value = azurerm_storage_account.sa3.id
 }
